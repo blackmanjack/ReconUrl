895c1b1cc9276de8cde5550f1faead62279185b9
snvm <faiz_m17@apps.ipb.ac.id>
2025-06-16 12:43:15 +0700

    refactor(root): refactor root endpoint

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import OtxApp from "../OtxApp";
+import OtxApp from "../AlienVault/OtxApp";
 import TesXss from "../xss";
 import Login from "../New-path/login";
+import PunycodeGenerator from "../PunyCode";
+import Home from "../Home";
 
 const SwitchRouter = () => {
   return (
@@ -12,7 +14,9 @@ const SwitchRouter = () => {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/xss" element={<TesXss />} />
-          <Route path="/" element={<OtxApp />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/alienvault" element={<OtxApp />} />
+          <Route path="/punycode" element={<PunycodeGenerator />} />
         </Routes>
       </Router>
     </>
